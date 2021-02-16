a1a45279ee653584e80a6fd7cd15fec76a354689
rvrn2hdp <rvrn2hdp@gmail.com>
2021-02-16 15:15:48 -0300

    a

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from bdb import foo
 
 # Create your views here.
 
 def noticia_list(request):
-    return render(request, 'ejemplo/post_list.html', {})
+    
+    return render(request, 'ejemplo/noticia_list.html', {})
--- miproyecto/urls.py
+++ miproyecto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ejemplo.views import noticia_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('noticia_list/', noticia_list),
 ]
